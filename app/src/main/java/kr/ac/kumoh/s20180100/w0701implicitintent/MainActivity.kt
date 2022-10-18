f41d725d23756303b13de322f04f7a84c9a02745
BomuKim <styg0604@gmail.com>
2022-10-18 14:32:53 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20180100/w0701implicitintent/MainActivity.kt
@@ -0,0 +1,22 @@
+package kr.ac.kumoh.s20180100.w0701implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.s20180100.w0701implicitintent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.btnHomepage.setOnClickListener {
+            val uri = Uri.parse("https://github.com/styg0604")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+    }
+}
